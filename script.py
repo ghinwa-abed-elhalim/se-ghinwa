1b8e32c8d0a5dbb5bcf93a13a3dfbf610eacfe77
ghinwa-abed-elhalim <ghenwaah77@gmail.com>
2025-02-13 23:52:09 +0000

    added if

--- script.py
+++ script.py
@@ -1,3 +1,13 @@
 print("welcome to the movie cinema! ")
 age = int(input("please insert your age "))
 bill = 0 
+
+if age <= 10:
+    print("ticket price is 3$ ")
+
+elif age <= 15 and age > 10:
+    print("ticket price is 5$")
+
+else:
+    print("ticket price is 8$")
+
