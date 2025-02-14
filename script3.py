d49be4f73a97455c3845f53e2315242fb347e65a
ghinwa-abed-elhalim <ghenwaah77@gmail.com>
2025-02-14 00:33:26 +0000

    fixed error string

--- script3.py
+++ script3.py
@@ -2,7 +2,7 @@ print ("hello there! ")
 username = input("username: ")
 password = input("password: ")
 
-if username == "admin" and password == 1234:
+if username == "admin" and password == "1234":
     print("access granted ")
 else:
     print("access denied ")
